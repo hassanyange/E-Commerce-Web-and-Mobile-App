b5fec6756a1fc5209eab757538191c33cd18dca2
hassanyange <hassanyange@gmail.com>
2024-05-26 03:49:24 -0700

    some changes

--- main/forms.py
+++ main/forms.py
@@ -5,3 +5,8 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ['name', 'email', 'location', 'picture', 'phone_number']
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'picture': forms.FileInput(attrs={'class': 'form-control-file'})
+        }
--- main/models.py
+++ main/models.py
@@ -30,6 +30,9 @@ class Customer(models.Model):
     picture = models.ImageField(upload_to='static/images/', default='default.jpg')
     phone_number = models.CharField(max_length=20, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     category = models.CharField(max_length=30)
